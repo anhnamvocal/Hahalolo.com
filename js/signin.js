d7fc47fbbd216f038ab5b014cacd1d7ebca640d6
anhnamvocal <Willumpd1@gmail.com>
2022-01-08 09:36:22 +0700

    finish login

--- js/signin.js
+++ js/signin.js
@@ -1,25 +1,15 @@
-let password = document.getElementById('password');
-let showEye = document.getElementById('showEye');
-let hideEye = document.getElementById('hideEye');
+// step 1
+const ipnElement = document.querySelector('#txtPass')
+const btnElement = document.querySelector('#btn-pass')
 
-function black(){
-  showEye.style.fill = "#000000";
-  hideEye.style.fill = "#000000";
-}
-function white(){
-  showEye.style.fill = "#fff";
-  hideEye.style.fill = "#fff";
-}
+// step 2
+btnElement.addEventListener('click', function() {
+  // step 3
+  const currentType = ipnElement.getAttribute('type')
+  // step 4
+  ipnElement.setAttribute(
+    'type',
+    currentType === 'password' ? 'text' : 'password'
+  )
+})
 
-function passwordShow(){
-  password.type = 'text';
-  showEye.style.display= "none";
-  hideEye.style.display= "inline";
-  password.focus();
-}
-function passwordHide(){
-  password.type = 'password';
-  showEye.style.display= "inline";
-  hideEye.style.display= "none";
-  password.focus();
-}
