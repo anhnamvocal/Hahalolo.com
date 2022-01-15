269c52e7c22a0f3496a2b0458055f7d0fa864f41
ghostorya3 <ghostorya3@gmail.com>
2022-01-15 22:45:06 +0700

    final

--- js/index.js
+++ js/index.js
@@ -1,19 +1,59 @@
-        var buttons = document.getElementsByClassName('tablinks');
-        var contents = document.getElementsByClassName('tabcontent');
-        
-        function reply_click(clicked_name)
-        {
-            var btn_color = document.getElementsByName(clicked_name);
-            for (var i = 0; i < buttons.length; i++) {
-                buttons[i].classList.remove("active");
-            }
+var buttons = document.getElementsByClassName('tablinks');
+var contents = document.getElementsByClassName('tabcontent');
+var id_news = document.getElementById('news');
+var id_ex = document.getElementById('ex');
+var index_light = document.getElementById('id-index-view');
+var header = document.getElementsByTagName('html')
+function reply_click(clicked_name)
+{
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("active");
+    }
+    this.className += " active";
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].style.display = 'none';
+    }
+    var content = document.getElementsByClassName(clicked_name);
+    for (var i = 0; i < contents.length; i++) {
+        content[i].style.display = 'block';
+    }
+    index_light.className += " my-light";
+    header.className += " my-light";
+}
 
-            btn_color.className += " active"
-            for (var i = 0; i < contents.length; i++) {
-                contents[i].style.display = 'none';
-            }
-            var content = document.getElementById(clicked_name);
-            content.style.display = 'block';
-            
-        }
-        
+
+function countChar(val) {
+    var len = val.value.length;
+    if (len >= 1500) {  
+      val.value = val.value.substring(0, 1500);
+    } else {
+      $('#charNum').text(len);
+    }
+  };
+
+  
+$(document).ready(function() {
+  $('#image').change(function(){
+      var file_data = $('#image').prop('files')[0];   
+      var form_data = new FormData();                  
+      form_data.append('image', file_data);
+      $.ajax({
+          url: "upload.php",
+          type: "POST",
+          data: form_data,
+          contentType: false,
+          cache: false,
+          processData:false,
+          success: function(data){
+            $("#targetLayer").html(data);
+              },
+              error: function()
+              {
+              },
+              complete: function (data) {
+                $('#uploadForm')[0].reset(); // this will reset the form fields
+            } 	        
+      });
+  });
+});
+  
